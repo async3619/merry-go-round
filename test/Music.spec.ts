9005e92e2f9b102f67193258cdb23b5ee8f291e7
Sophia <async3619@naver.com>
2019-04-13 06:46:52 +0900

    rewrote test code

--- test/Music.spec.ts
+++ test/Music.spec.ts
@@ -1,19 +1,21 @@
 import * as path from "path";
+import * as fs from "fs";
 import { assert, expect } from "chai";
 
 import * as merryGoRound from "../src";
+import { NativeMusic } from "../src/types";
 
 const SAMPLES_PATH = path.resolve(__dirname, "./samples");
+let music: NativeMusic | null = null;
 
 describe("Music class", () => {
     it("should load media file through `create` method", () => {
-        const music = merryGoRound.loadFromFile(path.resolve(SAMPLES_PATH, "Witness-06-Farewell.mp3"));
+        music = merryGoRound.loadFromFile(path.resolve(SAMPLES_PATH, "Witness-06-Farewell.mp3"));
 
         assert(music);
     });
 
     it("should parse tag information from media file", () => {
-        const music = merryGoRound.loadFromFile(path.resolve(SAMPLES_PATH, "Witness-06-Farewell.mp3"));
         const tagInformation = {
             title: music.title(),
             artist: music.artist(),
@@ -33,22 +35,32 @@ describe("Music class", () => {
         });
     });
 
-    /*
     it("should be able to get media file type", () => {
-        const music = merryGoRound.loadFromFile(path.resolve(SAMPLES_PATH, "Witness-06-Farewell.mp3"));
-        expect(music.type()).to.equal("MPEG");
+        expect(music.fileType()).to.equal("MPEG");
     });
-     */
 
     it("should be able to get media tag type", () => {
-        const music = merryGoRound.loadFromFile(path.resolve(SAMPLES_PATH, "Witness-06-Farewell.mp3"));
         expect(music.tagType()).to.equal("ID3v2.3");
     });
 
     it("should be able to get raw tag data of media file", () => {
-        const music = merryGoRound.loadFromFile(path.resolve(SAMPLES_PATH, "Witness-06-Farewell.mp3"));
         assert(music.nativeData());
     });
 
-    it("should be able to read album cover image data of media file", () => { return; });
+    it("should be able to read album cover image data of media file (ID3v2::APIC)", () => {
+        const nativeData = music.nativeData();
+        if (nativeData.dataType === "ID3v2") {
+            assert(nativeData.APIC);
+            if (!Array.isArray(nativeData.APIC)) {
+                expect(nativeData.APIC.type.id).to.be.equal(0x03);
+                expect(nativeData.APIC.type.description).to.be.equal("Cover (front)");
+                expect(nativeData.APIC.description).to.be.equal("");
+                expect(nativeData.APIC.mimeType).to.be.equal("image/jpeg");
+                assert(nativeData.APIC.data instanceof Buffer);
+
+                const buffer = fs.readFileSync(path.resolve(SAMPLES_PATH, "./Witness-06-Farewell.cover.jpeg"));
+                assert(buffer.equals(nativeData.APIC.data));
+            }
+        }
+    });
 });
