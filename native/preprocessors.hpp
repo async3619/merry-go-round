47e18416c4f19b50fbabe9f5aceb272fe181c0ba
Sophia <async3619@naver.com>
2019-04-14 16:24:10 +0900

    make project native source code unix-compliant

--- native/preprocessors.hpp
+++ native/preprocessors.hpp
@@ -1,12 +1,14 @@
 #ifndef MERRY_GO_ROUND_PREPROCESSORS_HPP
 #	define MERRY_GO_ROUND_PREPROCESSORS_HPP
 #	if defined(_WIN32)
+#		define IO_H <io.h> 
 #		if defined(MERRY_GO_ROUND_USE_WIN32) || !defined(MERRY_GO_ROUND_USE_POSIX)
 #			define _MERRY_GO_ROUND_USE_WIN32 
 #		elif defined(MERRY_GO_ROUND_USE_POSIX)
 #			define _MERRY_GO_ROUND_USE_POSIX
 #		endif
 #	else
+#		define IO_H <sys/io.h> 
 #		define _MERRY_GO_ROUND_USE_POSIX
 #	endif
 
@@ -16,14 +18,14 @@
 #			define _CRT_SECURE_NO_DEPRECATE
 #			define _CRT_NONSTDC_NO_WARNINGS
 #			include <sys/stat.h>
-#			include <io.h>
+#			include IO_H
 #			include <fcntl.h>
 #		endif
 #		define WIN32_LEAN_AND_MEAN
 #		include <windows.h>
 #	else
 #		include <sys/stat.h>
-#		include <io.h>
+#		include IO_H
 #		include <fcntl.h>
 #	endif
 #endif // MERRY_GO_ROUND_PREPROCESSORS_HPP
